76557cd6c84eb4c00cfafc55a46372988a31283e
Cagil3009 <116015987+Cagil3009@users.noreply.github.com>
2023-10-28 17:35:30 +0200

    structure: NUM, MINUS, PLUS, MULT, DIV

---
+++ src/main/scala/Enums.scala
@@ -0,0 +1,21 @@
+object Enums {
+
+  enum ArithmeticExpression {
+    case NUM, MINUS, PLUS, MULT, DIV
+  }
+  //val num: Int = ArithmeticExpression.NUM
+  val minus: ArithmeticExpression = ArithmeticExpression.MINUS
+  val plus: ArithmeticExpression = ArithmeticExpression.PLUS
+  val mult: ArithmeticExpression = ArithmeticExpression.MULT
+  val div: ArithmeticExpression = ArithmeticExpression.DIV
+
+
+  @main def hello() = {
+    //println("Hello, World")
+  }
+}
+  //def main(args: Array[String]) : Unit = {
+
+
+
+
